ff013669966c7faaf1605898730156902589c42c
Adeshina Ibrahim <debowale01@gmail.com>
2021-12-07 17:37:30 +0100

    displaying products on the home page

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Card, Button} from 'react-bootstrap'
+import Rating from './Rating'
+
+const Product = ({product}) => {
+  return (
+    <Card className='my-3 p-3 rounded' style={{ width: '18rem' }}>
+      <a href={`/product/${product._id}`}>
+        <Card.Img variant="top" src={product.image} />
+      </a>
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as='div'>{product.name}</Card.Title>
+        </a>
+        <Card.Text as='div'>
+          <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+        </Card.Text>
+        <Card.Text as='h3'>
+            ${product.price}          
+        </Card.Text>
+        <Button variant="primary">Add To Cart</Button>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default Product
