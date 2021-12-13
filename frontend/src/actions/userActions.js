c3037d587f8ef004522f67e027ad4fad8d1a837b
Adeshina Ibrahim <debowale01@gmail.com>
2021-12-13 13:51:55 +0100

    get user profile

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -52,3 +52,26 @@ export const register  = (name, email, password) => async (dispatch) => {
       payload:  error.response && error.response.data.message ? error.response.data.message : error.message })
   }
 }
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: userConstants.USER_DETAILS_REQUEST })
+    const { userLogin: { userInfo } } = getState()
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    }
+    const {data} = await axios.get(`/api/users/${id}`, config)
+    dispatch({
+      type: userConstants.USER_DETAILS_SUCCESS, 
+      payload: data
+    })
+  } catch (error) {
+    dispatch({ 
+      type: userConstants.USER_REGISTER_FAIL, 
+      payload:  error.response && error.response.data.message ? error.response.data.message : error.message })
+  }
+
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
-import { userLoginReducer, userRegisterReducer } from './reducers/userReducers'
+import { userLoginReducer, userRegisterReducer, userDetailsReducer } from './reducers/userReducers'
 import { cartReducer } from './reducers/cartReducers'
 
 
@@ -12,6 +12,7 @@ const reducers = combineReducers({
   cart:           cartReducer,
   userLogin:      userLoginReducer,
   userRegister:   userRegisterReducer,
+  userDetails:   userDetailsReducer,
 
 })
 
