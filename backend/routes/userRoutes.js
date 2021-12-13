f91793e8f730cc223bc21916628926ffe85ab8df
Adeshina Ibrahim <debowale01@gmail.com>
2021-12-13 12:34:00 +0100

    update user profile endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { authUser, getUserProfile, registerUser } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js'
 // import { registerUser, getAllUsers, authUser, getUserProfile, updateUserProfile, deleteUser, getUserById, updateUser } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
@@ -9,7 +9,7 @@ router.route('/').post(registerUser)
 router.post('/login', authUser)
 
 // router.use(protect)
-router.route('/profile').get(protect, getUserProfile)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
 // router.route('/').get(protect, restrictTo('admin'), getAllUsers).post(registerUser)
 
