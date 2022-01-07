1d017527d2ada856c0c9deac256d418b3c96afc9
Adeshina Ibrahim <debowale01@gmail.com>
2022-01-07 11:46:59 +0100

    admin create and update product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -45,3 +45,52 @@ export const deleteProduct = asyncHandler(async (req, res) => {
     throw new Error('Product Not Found')
   }
 })
+
+/**
+ * @desc  Create single product
+ * @route POST /api/products
+ * @access  private/Admin
+ */
+ export const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample Name',
+    price: 0,
+    image: '/images/sample.jpg',
+    brand: 'sample brand',
+    category: 'sample category',
+    name: req.body.name,
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample Description'
+  })
+
+  const newProduct = await product.save()
+  res.status(201).json(newProduct)
+})
+
+/**
+ * @desc  Update single product
+ * @route PUT /api/product/:id
+ * @access  private/Admin
+ */
+ export const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, image, countInStock, brand, category, description } = req.body
+
+  const product = await Product.findById(req.params.id)
+  if(product){
+    product.name = name
+    product.price = price
+    product.image = image
+    product.countInStock = countInStock
+    product.brand = brand
+    product.category = category
+    product.description = description
+
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+  }else{
+    res.status(404)
+    throw new Error('Prooduct not found!')
+  }
+
+})
